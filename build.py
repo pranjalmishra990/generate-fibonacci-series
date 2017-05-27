84f920fcbbf5c09b1d1b79e19a0f3ca96602be40
Sangam Angre <sangam.angre@gmail.com>
2017-05-27 17:10:04 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,12 +1,1 @@
-num1 = 0
-num2 = 1
-count = 2
-print(num1)
-print(num2)
-while count < 10:
-    next_fibonacco =  num1 + num2
-    print (next_fibonacco)
-    num1 = num2
-    num2 = next_fibonacco
-    count += 1
 
